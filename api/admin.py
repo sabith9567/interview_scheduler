05a0690bbadfce41c62f2014190d40a17b8b89a2
SABITH <SADITH@SABITHs-MacBook-Pro.local>
2021-02-28 20:21:37 +0530

    added model admin

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from api.models import *
 
 # Register your models here.
+
+class UsersAdmin(BaseUserAdmin):
+    list_display = ['id', "username", "is_interviewer"]
 
 admin.site.register(CustomUser)
 admin.site.register(TimeSlot)
